c5dc3a6dc3adcf21c76175421c6ad9ba8fb7c6ed
Daoyi Li <52638056+smallelephant9516@users.noreply.github.com>
2022-05-30 14:26:07 -0400

    Update seq_align.py

--- seq_align.py
+++ seq_align.py
@@ -11,12 +11,13 @@ def align_sequence(s0, s1):
     #DP, matrix[i][j] is the number of index that aligned between s0[:i],s1[:j]
     for i in range(1, s0_len + 1):
         for j in range(1, s1_len + 1):
-            if x[i - 1] == y[j - 1]:
-                matrix[i][j] = matrix[i - 1][j - 1] + 1
+            if x[i-1] == y[j-1]:
+                matrix[i][j] = matrix[i-1][j-1] + 1
             else:
-                matrix[i][j] = max(matrix[i][j - 1], matrix[i - 1][j])
+                matrix[i][j] = max(matrix[i][j-1], matrix[i-1][j])
     # bottom up to find aligned sequence 
     aligned_answer=[]
+    aligned_index=[]
     maxi=s0_len
     maxj=s1_len
     for i in range(s0_len,0, -1):
@@ -29,11 +30,13 @@ def align_sequence(s0, s1):
                 continue
             if (matrix[i][j]>matrix[i][j-1]) & (matrix[i][j]>matrix[i-1][j]):
                 aligned_answer.append(x[i-1])
+                aligned_index.append(i-1)
                 print(i,j,matrix[i][j])
                 maxi=i
                 maxj=j
     aligned_answer.reverse() #reverse the sequence of list, a function in python
-    return matrix[-1][-1], aligned_answer
+    aligned_index.reverse()
+    return matrix[-1][-1], aligned_answer, aligned_index
 
 
 
@@ -42,6 +45,7 @@ b=[1,2,3,4,5,6,7,8]
 
 #a=[5,4,3,2,1]
 #b=[1,2,3,4,5]
-number,answer=align_sequence(b,a)
+number,answer,index=align_sequence(b,a)
 print('Total aligned sequence are',number)
 print('The result that aligned to the correct answers are',answer)
+print('The index that aligned to the correct answer are',index)
